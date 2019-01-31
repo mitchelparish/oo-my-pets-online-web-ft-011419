7bd8d3fc65b3d98c751053563606b1448a202237
Mitchel Parish <mitchelparish@gmail.com>
2019-01-31 00:21:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,9 +3,19 @@ class Owner
   attr_reader :species
   attr_accessor :pets
 
-  def initialize
+  def initialize(species)
     @species = species
     @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def buy_cat(name)
+    pets[:cat] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+  end
+
+  def buy_cat(name)
   end
 
 
