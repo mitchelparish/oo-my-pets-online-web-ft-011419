0e569e246d4441055977aeacd32653e925980a14
Mitchel Parish <mitchelparish@gmail.com>
2019-01-31 00:05:52 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,13 @@
 class Cat
   attr_reader :name, :mood
 
-  def initialize(name, mood)
+  def initialize(name)
     @name = name
-    @mood = mood 
   end
 
+  def mood(mood)
+    @mood = mood
+  end 
 
 
 
