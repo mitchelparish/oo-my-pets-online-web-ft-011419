30e7f38810006cf4c6c809938116f69f49674ce9
Mitchel Parish <mitchelparish@gmail.com>
2019-01-31 00:27:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,6 +30,18 @@ class Owner
     end
   end
 
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
 
 
 
